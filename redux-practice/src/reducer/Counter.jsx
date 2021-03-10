3e051416405aa2afc153111cf9a47d5e8a0823ab
Johnathan <jonathanumana86@gmail.com>
2021-03-10 16:25:09 -0500

    Made Counter reducer in Reducer

---
+++ redux-practice/src/reducer/Counter.jsx
@@ -0,0 +1,13 @@
+const counter = (state = 0, action) => {
+    switch (action.type){
+        case "INCREASE":
+            return state++
+
+        case "DECREASE":
+            return state--
+
+        default:
+            return state
+    }
+
+}
