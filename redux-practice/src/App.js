0a9b4b8d6f4fc650051ab3f93cac86e814bdee36
Johnathan <jonathanumana86@gmail.com>
2021-03-16 19:43:04 -0400

    Got Counter state and useSelector for render on to the DOM

--- redux-practice/src/App.js
+++ redux-practice/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import '../src/components/Style.css'
+import { useSelector } from 'react-redux'
 
 function App() {
+
+  const counter = useSelector(state => state.Counter)
+
+
   return (
     <div className="App">
       <h1>REDUX</h1>
+      <h3>Counter {counter}</h3>
     </div>
   );
 }
