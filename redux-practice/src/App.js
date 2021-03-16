53106753ab3e58bafb1a314dd7c80482a4e1c9cf
Johnathan <jonathanumana86@gmail.com>
2021-03-16 19:51:13 -0400

    Connecting the buttons with Action so counter can increase or decrease

--- redux-practice/src/App.js
+++ redux-practice/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../src/components/Style.css'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { increase,  decrease } from './action/Action'
 
 function App() {
 
@@ -11,6 +12,8 @@ function App() {
     <div className="App">
       <h1>REDUX</h1>
       <h3>Counter {counter}</h3>
+      <button>➕</button>
+      <button>➖</button>
     </div>
   );
 }
